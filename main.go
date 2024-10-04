6d5aa53f96c25be709001efa801e24101c5032f5
Andrey Volobuev <avvolob@gmail.com>
2024-10-04 12:23:11 +0300

    Added func to create trips

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ import (
 	"gorm.io/gorm"
 )
 
-func HandleGetTrips(w http.ResponseWriter, r *http.Request) {
+const (
+	METHOD_NOT_ALLOWED = "[%s] METHOD IS NOT ALLOWED!"
+
+	// headers
+	CONTENT_TYPE     = "Content-Type"
+	APPLICATION_JSON = "application/json"
+)
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
 	dsn := "host=localhost user=aerolist password=aerolist dbname=aerolist port=5432"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -46,13 +54,35 @@ func HandleGetTrips(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, string(str))
 }
 
-func main() {
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "{\"status\": \"post_ok\"}")
+}
 
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "{\"status\": \"delete_ok\"}")
+}
+
+func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+
+	switch r.Method {
+	case http.MethodPost:
+		handlePost(w, r)
+	case http.MethodGet:
+		handleGet(w, r)
+	case http.MethodDelete:
+		handleDelete(w, r)
+	default:
+		log.Printf(METHOD_NOT_ALLOWED, r.Method)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
-	http.HandleFunc("/", HandleGetTrips)
+	http.HandleFunc("/", handleHTTP)
 	http.ListenAndServe("localhost:9000", nil)
 }
